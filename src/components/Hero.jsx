5d28a8834f809d960bef9a07d164a6d940b6e373
sumiya-31 <sumiya3131@gmail.com>
2024-12-31 13:14:26 +0530

    code updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -57,39 +57,3 @@ export default Hero;
 
 
 
-
-
-// // // import React from 'react'
-// import aboutme from '../assets/aboutme.jpeg'
-// const Hero = () => {
-//   return (
-//     <div className="bg-black text-white text-center py-16">
-//       <img src={aboutme} alt="" 
-//        className="mx-auto mb-8 w-45 h-45 rounded-full object-cover transform 
-//           transition-transform duration-300 hover:scale-105"
-//       />
-//       <h1 className="text-4xl font-bold">
-//         I am {""}
-//         <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500">SHAIK.SUMIYA</span>
-//         ,full-stack Developer
-//       </h1>
-//       <p className="mt-4 text-lg text-gray-300">
-//          i specialize in building and responsive web applications.
-//       </p>
-//       <div className="mt-8 space-x-4">
-//         <button
-//         className="bg-gradient-to-r from-green-400 to-blue-500 text-white
-//         transform transition-transform duration-300 hover:scale-105 px-6 py-2 rounded-full">
-//         contact me</button>
-//         <button 
-//         className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white
-//           transform transition-transform duration-300 hover:scale-105 px-6 py-2 rounded-full">
-//         Resume
-//        </button>
-        
-//       </div>
-//     </div>
-//   );
-// }
-// ;
-// export default Hero
